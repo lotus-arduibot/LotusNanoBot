c36704c526dec79229e8290a42152dea3f431abb
Lotus-Arduibot <82136154+lotus-arduibot@users.noreply.github.com>
2021-04-19 22:33:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     title : 'LotusNanoBOT',
     description : 'บอร์ดการเรียนรู้หุ่นยนต์และโครงงานระบบอัตโนมัติ',
     author : 'Lotus-arduibot',
-    website : http://www.lotus-arduibot.com/product/213/%E0%B8%8A%E0%B8%B8%E0%B8%94%E0%B8%AB%E0%B8%B8%E0%B9%88%E0%B8%99%E0%B8%A2%E0%B8%99%E0%B8%95%E0%B9%8C%E0%B9%80%E0%B8%9E%E0%B8%B7%E0%B9%88%E0%B8%AD%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%80%E0%B8%A3%E0%B8%B5%E0%B8%A2%E0%B8%99%E0%B8%A3%E0%B8%B9%E0%B9%89-lotus-mini-nano/',
+    website : 'http://www.lotus-arduibot.com/product/213/%E0%B8%8A%E0%B8%B8%E0%B8%94%E0%B8%AB%E0%B8%B8%E0%B9%88%E0%B8%99%E0%B8%A2%E0%B8%99%E0%B8%95%E0%B9%8C%E0%B9%80%E0%B8%9E%E0%B8%B7%E0%B9%88%E0%B8%AD%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%80%E0%B8%A3%E0%B8%B5%E0%B8%A2%E0%B8%99%E0%B8%A3%E0%B8%B9%E0%B9%89-lotus-mini-nano/',
     email : 'lotusarduibot@gmail.com',
     git : 'https://github.com/lotus-arduibot/LotusNanoBot/',    
     image : '/static/display.jpg',
