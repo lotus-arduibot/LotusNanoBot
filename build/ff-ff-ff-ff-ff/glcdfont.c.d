018841dc09b45541cc6d08382f2a8ca4e59096f0
Lotus-Arduibot <82136154+lotus-arduibot@users.noreply.github.com>
2021-04-19 20:48:12 +0700

    Delete build/ff-ff-ff-ff-ff directory

--- build/ff-ff-ff-ff-ff/glcdfont.c.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Administrator\AppData\Local\Programs\kbide\boards\LotusNanoBot\build\ff-ff-ff-ff-ff\glcdfont.c.o: \
- C:\Users\Administrator\AppData\Local\Programs\kbide\boards\LotusNanoBot\include\glcdfont.c
